277d6a54f07d187f04404f925cffdb531e1424bc
DanielSerio <dserio.d@gmail.com>
2025-08-05 10:39:28 -0400

    added database config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,33 @@ import { CategoriesModule } from './categories/categories.module';
 import { SubcategoriesModule } from './subcategories/subcategories.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './categories/entities/category.entity';
+import { Subcategory } from './subcategories/entities/subcategory.entity';
 
 @Module({
-  imports: [CategoriesModule, SubcategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'better-sqlite3',
+        database: configService.get('DATABASE'),
+        entities: [
+          Category,
+          Subcategory
+        ],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    CategoriesModule,
+    SubcategoriesModule
+  ],
   providers: [
     {
       provide: APP_PIPE,
